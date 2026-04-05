2ad4e6d9155407f7dd4806e787e99203dc7e4b86
Tejas <tejasdilipkamble@gmail.com>
2026-04-05 13:18:37 +0530

    luh ahh animations

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.from(".title",{
+    y:50,
+    opacity :0,
+    duration :1,
+    ease : "power2.out"
+});
+
+gsap.from(".start-btn",{
+    y:20,
+    opacity:0,
+    delay :0.5,
+    duration:0.8
+});
